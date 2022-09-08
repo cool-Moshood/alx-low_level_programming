2ffd0ba395f9e342cef32fa09ec44135a85539f1
cool-Moshood <moshoodage@gmail.com>
2022-09-08 15:43:55 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * sizeof of varoius types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %d bytes(s)\n", sizeof(char));
+printf("size of a int: %d bytes(s)\n", sizeof(int));
+printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("size of a float: %d bytes(s)\n", sizeof(float));
+
+return (0);
+}
