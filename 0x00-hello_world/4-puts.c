0c51edf7608c2eec9dd1a6042a4a148bdb168913
cool-Moshood <moshoodage@gmail.com>
2022-09-08 07:16:18 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using put
+ *
+ * Return: value 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
