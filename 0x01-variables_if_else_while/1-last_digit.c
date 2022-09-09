185595852177d6d1049f668df7ebfaa813ee1b4e
cool-Moshood <moshoodage@gmail.com>
2022-09-09 13:33:04 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'the program run radom number'
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int n, m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+m = n % 10;
+if (m > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, m);
+}
+else if (m == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, m);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+}
+return (0);
+}
