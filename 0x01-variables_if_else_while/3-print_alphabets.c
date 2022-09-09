ddea21042856e476c88970bb10e6bf59068a8eb8
cool-Moshood <moshoodage@gmail.com>
2022-09-09 15:42:42 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'the program run radom number'
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char x;
+
+	x = 'A';
+
+
+	while (x <= 'z')
+
+	{
+
+	    x = tolower(x);
+
+	    putchar(x);
+	    x++;
+	}
+
+	x = 'A';
+
+	while (x <= 'Z')
+
+	{
+
+	x = toupper(x);
+
+	putchar(x);
+	x++;
+	}
+	{
+	    putchar('\n');
+	}
+return (0);
+}
