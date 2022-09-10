538ee6ae9e65c7dcb9d92550298d31dd6da00ab3
cool-Moshood <moshoodage@gmail.com>
2022-09-09 20:56:47 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: 'using putchar'
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int x;
+
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar((x % 10) + '0');
+
+		if (x != 9)
+		{
+			putchar (',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
+}
