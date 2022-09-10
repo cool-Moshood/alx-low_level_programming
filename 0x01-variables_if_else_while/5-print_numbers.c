a52a55b4bca3401851fdd9a9a62342df48065ede
cool-Moshood <moshoodage@gmail.com>
2022-09-09 18:08:31 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+
+/**
+ *main - Entry point
+ *
+ *Description: 'the program that
+ * print single base number base 10'
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int x;
+
+	for (x = 0; x <= 9; x++)
+
+
+		printf("%u\n", x);
+
+
+	printf("\n");
+
+
+	return (0);
+}
