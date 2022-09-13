9953d50a5220a43b78874383ff037c9c59cfeb38
cool-Moshood <moshoodage@gmail.com>
2022-09-13 15:32:33 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * addition  of an two integer
+ *
+ * int: collect inter
+ * @x: number
+ * @y: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+	int add(int x, int y);
+	return (x + y);
+}
