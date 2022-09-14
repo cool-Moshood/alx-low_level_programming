3170cbadb9d0043d8b1749bf8ba7bb9249966e5a
cool-Moshood <moshoodage@gmail.com>
2022-09-14 14:59:13 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 89 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */#
+
+int main(void)
+
+{
+	long double t1 = 1;
+	long  double t2 = 2;
+	long  double nth = t1 + t2;
+	int x;
+
+	printf("%.0Lf, %.0Lf, ", t1, t2);
+
+	for (x = 3; x < 98; x++)
+	{
+
+		printf("%.0Lf, ",  nth);
+		t1 = t2;
+		t2 = nth;
+		nth = t1 + t2;
+
+	}
+	printf("%.0Lf\n", nth);
+	return (0);
+
+}
