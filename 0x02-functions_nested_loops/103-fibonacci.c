1bdb8f1502d2c34d4151761193b39fd6c65ffb9c
cool-Moshood <moshoodage@gmail.com>
2022-09-14 14:10:58 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	long int x = 1;
+	long int y = 2;
+	long int nth = x + y;
+	long sum = 0;
+
+	while (nth < 4000000)
+	{
+		if (nth % 2 == 0)
+		{
+			sum = nth + sum;
+		}
+		x = y;
+		y = nth;
+		nth = x + y;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
+
