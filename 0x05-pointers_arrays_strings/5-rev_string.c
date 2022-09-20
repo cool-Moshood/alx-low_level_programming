4ba3f4298038fc93d212da3d3ea5fc211e0f3918
cool-Moshood <moshoodage@gmail.com>
2022-09-19 18:14:30 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - fucntion that prints a string in reverse, then a new line
+ *
+ * @s: - pointer integer
+ *
+ * Return: Always 0 success.
+ */
+
+void rev_string(char *s)
+{
+	int x, y;
+
+	char z;
+
+	for (x = 0; s[x] != '\0'; ++x)
+
+	for (y = 0; y < x / 2; ++y)
+	{
+		z = s[y];
+
+		s[y] = s[x - 1 - y];
+
+		s[x - 1 - y] = z;
+	}
+}
