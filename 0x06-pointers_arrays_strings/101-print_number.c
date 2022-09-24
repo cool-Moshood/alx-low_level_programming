c754458a8db4fafde5963dcd87b7c0d323baa856
cool-Moshood <moshoodage@gmail.com>
2022-09-23 17:46:40 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar((x % 10) + '0');
+}
