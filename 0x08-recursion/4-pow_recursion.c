433d28fd7353111f0859bb0b78067ce37be125f0
cool-Moshood <moshoodage@gmail.com>
2022-09-28 19:29:03 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y.
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y.
+ *
  * @x: base.
  * @y: exponent.
+ *
  * Return: value of pow.
  */
 
