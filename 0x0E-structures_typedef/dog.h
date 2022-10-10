7051a654eb1cc37120cdcf85139769a5161f3e92
cool-Moshood <moshoodage@gmail.com>
2022-10-10 15:21:36 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for a dog object
+ * @name:  dog name
+ * @age:   dog age
+ * @owner: dog 0wner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
