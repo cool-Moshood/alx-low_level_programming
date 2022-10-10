fb9cc577cdc3167f650447d25cc8f496ea08f14a
cool-Moshood <moshoodage@gmail.com>
2022-10-10 12:20:00 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - program prints the name of the file
+ * @viod: 0
+ * Return: 0
+ */
 
-int main() {
+int main(viod)
+{
 
 	printf("%s", __FILE__);
 
-	return 0;
+	return (0);
 }
