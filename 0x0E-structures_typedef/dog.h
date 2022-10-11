86f2f8881cdc8b53c600eabd8e5888826a6da8c3
cool-Moshood <moshoodage@gmail.com>
2022-10-10 18:30:51 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,17 @@
 #define DOG_H
 /**
  * struct dog - structure for a dog object
- * dog_t - typedef
  * @name:  dog name
  * @age:   dog age
  * @owner: dog 0wner
  */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+typedef struct dog dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
