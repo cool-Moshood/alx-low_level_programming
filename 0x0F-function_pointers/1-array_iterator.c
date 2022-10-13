7e63eac032931771580b02f22c120a8805352f3c
cool-Moshood <moshoodage@gmail.com>
2022-10-12 18:46:55 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (array || action)
+	if (array || size || action)
 	{
 		for (x = 0; x < size; x++)
-
 			action(array[x]);
 	}
 }
