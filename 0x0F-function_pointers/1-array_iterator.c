8c8a4f4fcd70fc72edd81f23469e382514bcd532
cool-Moshood <moshoodage@gmail.com>
2022-10-12 18:40:32 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that prints an integer
+ *
+ * @array: pointer
+ * @action: pointer to the function
+ * @size:  size of the array
+ *
+ * Return: Nothing.
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	if (array || action)
+	{
+		for (x = 0; x < size; x++)
+
+			action(array[x]);
+	}
+}
