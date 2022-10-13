c5ab3a5dcb37aab009cb625e057d750eb35f0bda
cool-Moshood <moshoodage@gmail.com>
2022-10-12 17:59:02 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name: person
+ * @f: pointer to the person name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
