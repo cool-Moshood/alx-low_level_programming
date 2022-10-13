ad47b4911663c45bfebb7695d908e8dc626fce79
cool-Moshood <moshoodage@gmail.com>
2022-10-13 15:38:40 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,12 +18,8 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	va_start (ap, n);
 
-	sum_total = 0;
-
 	for (i = 0; i < n; i++)
-
 		sum_total += va_arg (ap, int);
-
 	va_end (ap);
 	return (sum_total);
 }
