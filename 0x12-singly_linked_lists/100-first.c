017ec08495cb24199697a0dcbe9f88860742fdcc
cool-Moshood <moshoodage@gmail.com>
2022-10-20 21:42:17 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - fucntion that printf before the main
+ *@void: nothing
+ * Return: 0.
+ */
+
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
