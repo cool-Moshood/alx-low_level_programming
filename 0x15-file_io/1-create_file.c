ec3da0ceabc82371383ebe85906eefd751d57d3c
cool-Moshood <moshoodage@gmail.com>
2022-10-31 16:55:19 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (ptr == -1)
 		return (-1);
