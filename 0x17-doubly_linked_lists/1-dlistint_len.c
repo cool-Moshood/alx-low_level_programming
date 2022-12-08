162e7994555ae051037fe542059bb3b0bc8e8e1d
cool-Moshood <moshoodage@gmail.com>
2022-12-08 22:40:49 +0100

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * distint_len - lenght of double linked list
+ * @h: head node
+ *
+ * Return: lenght of linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return count;
+}
