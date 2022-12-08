44f06552c2b5fdf4de46923f48b82423a00fe521
cool-Moshood <moshoodage@gmail.com>
2022-12-08 22:04:17 +0100

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print double linked list
+ * @h: head node
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node++;
+	}
+	return (nodes);
+}
