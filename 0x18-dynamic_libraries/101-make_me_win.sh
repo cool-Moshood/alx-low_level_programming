745dcc8e0ef1e14eb88d66cf1afbf77a8e8ca68b
cool-Moshood <moshoodage@gmail.com>
2022-12-12 22:33:54 +0100

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://raw.githubusercontent.com/cool-Moshood/alx-low_level_programming/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so
