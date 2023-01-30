cdad06cb319b571c64c1d6e5c5ddf9d2deb31f2f
cool-Moshood <moshoodage@gmail.com>
2023-01-30 05:21:52 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(sizeof(hash_node_t));
 
 	if (table->array == NULL)
 		return (NULL);
