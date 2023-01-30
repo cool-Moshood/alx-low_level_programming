50efe8b6479c69b97b8bcbbf26e3845f43d3502d
cool-Moshood <moshoodage@gmail.com>
2023-01-30 12:06:57 -0800

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,10 +9,10 @@
  * @size: size of hash table
  *
  * Return: given key index.
-*/
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
